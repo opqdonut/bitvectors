b9655b521d0ef1bd239d373716f6767e0147ca12
Joel Kaasinen <joel.kaasinen@gmail.com>
2010-02-11 21:24:04 +0200

    SizeRank monoid, rewrite tests

--- Tree.hs
+++ Tree.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE BangPatterns, MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE BangPatterns, MultiParamTypeClasses, FlexibleInstances,
+  ScopedTypeVariables #-}
 
 module Tree where
+
+import Util
 
 import Data.Monoid
 import Test.QuickCheck
@@ -8,6 +11,16 @@ import Test.QuickCheck.Property
 
 class Monoid a => Measured a v where
     measure :: v -> a
+
+{-
+instance (Measured a v, Measured b v)
+    => Measured (a,b) v where
+    measure x = (measure x, measure x)
+
+instance (Measured a v, Measured b v, Measured c v)
+    => Measured (a,b,c) v where
+    measure x = (measure x, measure x, measure x)
+-}
 
 (+++) :: Monoid a => a -> a -> a
 (+++) = mappend
@@ -29,11 +42,11 @@ node :: Measured ann val => Tree ann val -> Tree ann val
      -> Tree ann val
 node l r = Node Color (measure l +++ measure r) l r
 
-find :: Measured a v => (a -> Bool) -> Tree a v -> Maybe v
+find :: Measured a v => (a -> Bool) -> Tree a v -> Maybe (a,v)
 find p t = go mempty t
   where
     go acc (Leaf v)
-        | p (acc +++ measure v) = Just v 
+        | p (acc +++ measure v) = Just ((acc +++ measure v),v)
         | otherwise = Nothing
     go acc (Node _ ann l r)
         | p (acc +++ measure l) = go acc l
@@ -43,17 +56,41 @@ find p t = go mempty t
 
 -- -- -- Testing -- -- --
 
-instance Measured (Sum Int) Bool where
-    measure = const (Sum 1)
+data SizeRank = SizeRank {getSize :: Int, getRank :: Int}
 
-index i = (>i).getSum
+instance Monoid SizeRank where
+    mappend (SizeRank a a') (SizeRank b b') =
+        SizeRank (a+b) (a'+b')
+    mempty = SizeRank 0 0
+instance Measured SizeRank Bool where
+    measure True  = SizeRank 1 1
+    measure False = SizeRank 1 0
 
+index :: Int->SizeRank->Bool
+index i = (>i).getSize
+rank :: Int->SizeRank->Bool
+rank i = (>i).getRank
+
+mkbal :: [Bool] -> Tree SizeRank Bool
 mkbal [x] = Leaf x
 mkbal xs = let (a,b) = splitAt (length xs `div` 2) xs
            in node (mkbal a) (mkbal b)
 
-prop_mkbal :: [Bool] -> Gen Prop
-prop_mkbal xs =
-    not (null xs) ==>
-        forAll (choose (0,length xs-1))
-                   (\i -> find (index i) (mkbal xs) == Just (xs !! i))
+metaprop_mkbal p xs = not (null xs) ==>
+                      forAll (choose (0,length xs-1)) (p xs)
+
+prop_index :: [Bool] -> Gen Prop
+prop_index = 
+    metaprop_mkbal $ \xs i ->
+        case find (index i) (mkbal xs) of
+          Just ((SizeRank s _),b) -> b == xs !! i && s == i+1
+          Nothing -> False
+
+prop_rank :: [Bool] -> Gen Prop
+prop_rank =
+    metaprop_mkbal $ \xs i ->
+        case find (rank i) (mkbal xs) of
+          Just ((SizeRank s r),_) ->
+              count id (take (s-1) xs) == i
+          Nothing ->
+              i >= count id xs
