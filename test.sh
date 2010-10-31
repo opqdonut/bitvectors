3953ec918fb2bc13359297118d9f1103d26941a4
Joel Kaasinen <joel.kaasinen@gmail.com>
2010-10-31 13:11:10 +0200

    tune test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
-SIZE=$((1024*1024*2))
+SIZES="1000 10000 100000 1000000 10000000"
+QUERIES=100000
 
-echo $COMMAND 10 100000
+REPEATS=${1:-1}
+
+
+echo GIT: $(git log --oneline | head -1)
+echo REPEATS: $REPEATS
+echo LENGTHS: $COMMANDS 
 for COMMAND in "./test sg" "./test fd" "../impls/cst_v_1_0/mytest" \
     "../impls/rlcsa/mytest"; do
     echo $COMMAND " "
-    for i in 10 100000; do
-	/usr/bin/time -f " %e" $COMMAND $SIZE $i > /dev/null
+    for size in $SIZES; do
+	for j in $(seq $REPEATS); do
+	    echo -n $size
+	    /usr/bin/time -f " %e" $COMMAND $size $QUERIES > /dev/null
+	done
     done
 done
