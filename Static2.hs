b7a0fcbccdb255ec010e74104949db810a014402
Joel Kaasinen <joel.kaasinen@gmail.com>
2010-12-05 00:06:52 +0200

    SuccinctArray

--- Static2.hs
+++ Static2.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts, StandaloneDeriving #-}
+
 module Static2 where
 
 import Encoding2 hiding ((+++))
@@ -7,34 +9,74 @@ import BitVector
 
 import Data.Array.Unboxed
 import Data.Word
+import Data.List
 
 import Test.QuickCheck
 
 type V = UArray Int
 
+type Big = Int
+type Small = Word16
+
+data 
+  SuccinctArray =
+    SuccinctArray {stride :: Int,
+                   big :: V Big,
+                   small :: V Small}
+    deriving Show
+
+(!-) :: SuccinctArray -> Int -> Big
+(SuccinctArray stride big small) !- i =
+  base + fromIntegral offset
+  where (bigI,smallI) = i `divMod` stride
+        base = big ! bigI
+        offset = small ! (bigI*stride + smallI)
+
+mkSuccinctArray :: 
+  Int -> [Big] -> SuccinctArray
+mkSuccinctArray stride vals =
+  SuccinctArray stride big small
+    where (bigs,smalls) = loop 0 [] [] vals
+          small = listArray' (length smalls) smalls
+          big =   listArray' (length bigs)   bigs
+          a -. b = fromIntegral (a - b)
+          loop _    bigs smalls []       = (reverse bigs,reverse smalls)
+          loop 0    bigs smalls (v:vals) =
+            loop (stride-1) (v:bigs) (0:smalls) vals
+          loop left bigs smalls (v:vals) =
+            loop (left-1) bigs ((v -. head bigs) : smalls) vals
+
+prop_SuccinctArray :: Property
+prop_SuccinctArray =
+  forAll (listOf1 arbitrary) $ \vals ->
+  forAll (choose (1,length vals)) $ \stride ->
+  forAll (choose (0,length vals-1)) $ \i ->
+  let vals' = sort $ map fromIntegral (vals :: [Small])
+  in vals' !! i == mkSuccinctArray stride vals' !- i
+
 data Static =
   Static {compressed :: Block,
-          stride :: Int,
-          ranks :: V Int,      -- i -> rank(B,i*stride)
-          locations :: V Int,  -- \ mapping from unencoded locations
-          offsets :: V Int     -- / to encoded locations
+          blockSize :: Int,
+          ranks :: SuccinctArray,      -- i -> rank(B,i*blockSize)
+          locations :: SuccinctArray,  -- \ mapping from unencoded locations
+          offsets :: SuccinctArray     -- / to encoded locations
          }
   deriving Show
   
   
 mkStatic n bs = 
-  let stride = ilog2 n
-      arraySize = n `mydiv` stride
+  let blockSize = ilog2 n
+      stride = blockSize ^ 2
       compressed = gapBlock bs
-      p = process stride compressed
+      p = process blockSize compressed
       (encposes,offsets,sizeranks) = unzip3 p
       ranks = map getRank sizeranks
       
-      mkArr = listArray' arraySize
+      mkArr = mkSuccinctArray stride . map fromIntegral
           
   in Static
      compressed
-     stride
+     blockSize
      (mkArr ranks)
      (mkArr encposes)
      (mkArr offsets)
@@ -68,21 +110,21 @@ process stride block = format 0 (SizeRank 0 0) 0 $ loop 0 (SizeRank 0 0)
 
 _query :: Static -> Int -> Bool
 _query static i = 
-  let arrayIndex = i `div` stride static
-      i' = arrayIndex * stride static
-      location = locations static ! arrayIndex
-      offset = (offsets static ! arrayIndex) + (i-i')
+  let arrayIndex = i `div` blockSize static
+      i' = arrayIndex * blockSize static
+      location = locations static !- arrayIndex
+      offset = (offsets static !- arrayIndex) + (i-i')
       gaps = readEliass' (compressed static) location
   in
    queryGaps gaps offset
    
 _queryrank :: Static -> Int -> Int
 _queryrank static i =
-  let arrayIndex = i `div` stride static
-      i' = arrayIndex * stride static
-      location = locations static ! arrayIndex
-      offset = (offsets static ! arrayIndex) + (i-i')
-      baseRank = ranks static ! arrayIndex
+  let arrayIndex = i `div` blockSize static
+      i' = arrayIndex * blockSize static
+      location = locations static !- arrayIndex
+      offset = (offsets static !- arrayIndex) + (i-i')
+      baseRank = ranks static !- arrayIndex
       gaps = readEliass' (compressed static) location
   in
    baseRank + queryrankGaps gaps offset
