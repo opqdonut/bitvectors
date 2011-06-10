7feeea7bc7d5fd01392eeae5425a6efa2c82c8bd
Joel Kaasinen <joel.kaasinen@gmail.com>
2011-06-10 15:13:51 +0300

    slightly more robust memory.sh

--- memory.sh
+++ memory.sh
@@ -3,8 +3,8 @@ do
     echo '#' $TEST;
     for siz in 64 128 256 512
     do
-	./dist/build/Test/Test $TEST data/f32s1-${siz}k 100000 +RTS -hT >/dev/null
-	BYTES=$(awk -f process.awk < Test.hp | tail -5 | head -1 | cut -d' ' -f2)
+	./dist/build/Test/Test $TEST data/f32s1-${siz}k 200000 +RTS -hT >/dev/null
+	BYTES=$(awk -f process.awk < Test.hp | tail -4 | head -1 | cut -d' ' -f2)
 	ORIG=$(echo 1024 '*' $siz / 8 | bc)
 	RATIO=$(echo 'scale=7;' ${BYTES} / $ORIG | bc)
 	echo $siz $ORIG $BYTES $RATIO 
