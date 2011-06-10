25464405366f1264e73ab0738255f3da8e16b54a
Joel Kaasinen <joel.kaasinen@gmail.com>
2011-06-10 14:08:28 +0300

    memory.sh

---
+++ memory.sh
@@ -0,0 +1,12 @@
+for TEST in s2 fd fdn fun fs fse d dn ds dse
+do
+    echo '#' $TEST;
+    for siz in 64 128 256 512
+    do
+	./dist/build/Test/Test $TEST data/f32s1-${siz}k 100000 +RTS -hT >/dev/null
+	BYTES=$(awk -f process.awk < Test.hp | tail -5 | head -1 | cut -d' ' -f2)
+	ORIG=$(echo 1024 '*' $siz / 8 | bc)
+	RATIO=$(echo 'scale=7;' ${BYTES} / $ORIG | bc)
+	echo $siz $ORIG $BYTES $RATIO 
+    done
+done
